1bfc6dfafe0d97ac7fcb71267d92e124e98e095e
Adrian Carolli <adrian.carolli1@gmail.com>
2018-03-14 18:41:25 -0400

    Mongo setup

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://admin:alecia12@ds215019.mlab.com:15019/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
